26f06293af8fed0491fff6d01d1da4a0dd744a95
Stanley Ho <stanleyho862@gmail.com>
2024-01-18 16:24:56 -0800

    Slideshow and some rework and new images

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+
+    $(".prev").on("click", () => {
+        plusSlides(-1);
+    });
+    $(".next").on("click", () => {
+        plusSlides(1)
+    });
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        let i;
+        const slides = document.getElementsByClassName("slide");
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+    }
+});
